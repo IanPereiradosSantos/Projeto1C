ab2203a2c349c46b3c22f2b69397df36fbb6c91e
Ian Pereira dos Santos <155979621+IanPereiradosSantos@users.noreply.github.com>
2024-07-18 19:05:08 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Meu primeiro programa
+Autor: Ian Pereira dos Santos
+Data de criação: 17/07/2024
+IFSP
+*/
+
+int main() 
+{
+	printf("Meu primeiro programa.\n");
+	printf("BOLO DE CENOURA\n" 
+	"Massa:\n"
+	"1/2 xicara (cha) de oleo\n"
+	"4 ovos\n"
+	"2 e 1/2 xicaras (cha) de farinha de trigo\n"
+	"3 cenouras medias raladas\n"
+	"2 xicaras (cha) de acucar\n"
+	"1 colher (sopa) de fermento em po\n");
+	printf("Modo de preparo:\n" 
+	"Em um liquidificador, adicione a cenoura, os ovos e o oleo, depois misture.\n"
+	"Acrescente o acucar e bata novamente por 5 minutos.\n"
+	"Em uma tigela ou na batedeira, adicione a farinha de trigo e depois misture novamente.\n"
+	"Acrescente o fermento e misture lentamente com uma colher.\n"
+	"Asse em um forno preaquecido a 180 C por aproximadamente 40 minutos.");
+	return 0;
+}
